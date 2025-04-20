bf0995ccd95d4763e261b7adf819bfa87d00c252
SergeyRusak <rusak200281@gmail.com>
2025-04-20 17:10:24 +0300

    Fixed edit + added Docker Files

--- useless_client/app.py
+++ useless_client/app.py
@@ -140,4 +140,4 @@ def logout():
     return redirect(url_for("login"))
 
 if __name__ == '__main__':
-    app.run(port=5050, debug=True)
+    app.run( host="0.0.0.0", port=5050, debug=True)
--- web_client/main.py
+++ web_client/main.py
@@ -47,4 +47,4 @@ def get_note(note_id):
 
 
 if __name__ == "__main__":
-    app.run(port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
